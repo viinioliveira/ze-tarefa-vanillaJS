10812d26021648d565784b06c594fbf0fb758b90
Vincius <viniciusmeireles08@gmail.com>
2022-07-08 23:29:49 -0300

    first commit

---
+++ JS/main.js
@@ -0,0 +1,95 @@
+let tarefas = [];
+const tarefasAdicionadas = document.querySelector(".tarefas-adicionadas");
+//localStorage.setItem("tarefas", "[]");
+
+function adicionarTarefa() {
+  let tarefa = {
+    id: getMaiorId() + 1,
+    descricao: "",
+    validado: false,
+  };
+
+  tarefa.descricao = document.querySelector(".entrada-dados").value;
+  tarefasAdicionadas.innerHTML += `<div class="item-adicionado" id='${tarefa.id}'>
+    <input type="checkbox" class="check" id="checkbox${tarefa.id}">
+    <span>${tarefa.descricao}</span>
+    <button type="button" id="botao-editar" onclick="editar()">editar</button>
+    <button type="button" id="botao-excluir" onclick="excluir()">excluir</button>
+  </div>`;
+
+  document
+    .querySelector(`#checkbox${tarefa.id}`)
+    .addEventListener("change", (e) => {
+      validandoTarefa(tarefa.id, e.target.checked);
+    });
+
+  salvarTarefa(tarefa);
+}
+
+function getMaiorId() {
+  const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+  let maior = 0;
+  tarefas.forEach((element) => {
+    if (element.id > maior) {
+      maior = element.id;
+    }
+  });
+  return maior;
+}
+
+function salvarTarefa(tarefa) {
+  tarefas.push(tarefa);
+  localStorage.setItem("tarefas", JSON.stringify(tarefas));
+}
+
+console.log(tarefas);
+
+function recuperarDados() {
+  const tarefasConcluidas = document.querySelector(".tarefas-concluidas");
+  let dadosRecuperados = [];
+  dadosRecuperados = JSON.parse(localStorage.getItem("tarefas")) || [];
+
+  tarefasConcluidas.innerHTML = "";
+  tarefasAdicionadas.innerHTML = "";
+
+  dadosRecuperados.forEach((tarefa, index) => {
+    if (tarefa.validado) {
+      tarefasConcluidas.innerHTML += `<div class="item-adicionado" id='${tarefa.id}'>
+      <input type="checkbox" class="check" id="checkbox${tarefa.id}" checked>
+      <span>${tarefa.descricao}</span>
+      <button type="button" id="botao-editar" onclick="editar()">editar</button>
+      <button type="button" id="botao-excluir" onclick="excluir()">excluir</button>
+    </div>`;
+    } else {
+      tarefasAdicionadas.innerHTML += `<div class="item-adicionado" id='${tarefa.id}'>
+      <input type="checkbox" class="check" id="checkbox${tarefa.id}">
+      <span>${tarefa.descricao}</span>
+      <button type="button" id="botao-editar" onclick="editar()">editar</button>
+      <button type="button" id="botao-excluir" onclick="excluir()">excluir</button>
+    </div>`;
+    }
+    console.log("adicionando listen em #checkbox" + tarefa.id);
+    document
+      .querySelector(`#checkbox${tarefa.id}`)
+      .addEventListener("change", (e) => {
+        validandoTarefa(tarefa.id, e.target.checked);
+        console.log("pedrao em #checkbox" + tarefa.id);
+      });
+  });
+
+  tarefas = dadosRecuperados;
+}
+
+function validandoTarefa(idTarefa, validada) {
+  tarefas.forEach((tarefa) => {
+    if (tarefa.id === idTarefa) {
+      tarefa.validado = validada;
+    }
+  });
+  localStorage.setItem("tarefas", JSON.stringify(tarefas));
+  recuperarDados();
+
+  console.log("tarefas em validando tarefas", tarefas);
+}
+
+recuperarDados();
